e34ca94f49d8cf96ecd5fd559eb16487b234be20
eugenioclrc <eugenioclrc@gmail.com>
2025-10-30 20:40:39 -0300

    vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,4 @@
+/*
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
@@ -13,3 +14,19 @@ export default {
     })
   }
 };
+*/
+import adapter from '@sveltejs/adapter-vercel';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  preprocess: vitePreprocess(),
+	kit: {
+		adapter: adapter({
+			// see below for options that can be set here
+		})
+	}
+};
+
+export default config;
